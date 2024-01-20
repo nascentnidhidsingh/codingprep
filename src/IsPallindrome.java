e03518a8202783d1ac29c643fc034b6e3c024e5a
nascentnidhisingh <nascentnidhisingh@gmaile.com>
2024-01-20 12:13:38 +0530

    Is Pallindrome problem

---
+++ src/IsPallindrome.java
@@ -0,0 +1,28 @@
+//Given an integer x, return true if x is a
+//        palindrome
+//, and false otherwise.
+//
+//
+//
+//        Example 1:
+//
+//Input: x = 121
+//Output: true
+//Explanation: 121 reads as 121 from left to right and from right to left.
+class IsPallindrome {
+    public boolean isPalindrome(int x) {
+        //Time complexity-O(n)
+        //Space Complexity-O(1)
+        if (x < 0)
+            return false;
+
+        int reverse = 0;
+        int dup = x;
+        while (dup > 0) {
+            int mod = dup%10;
+            reverse = reverse*10+mod;
+            dup = (dup-mod)/10;
+        }
+        return x == reverse ? true : false;
+    }
+}

c1ff1f823d52c6002f29807d405ee42e779495a6
nascentnidhisingh <nascentnidhisingh@gmaile.com>
2024-02-08 01:26:50 +0530

    LevelOrder Traversal problem

---
+++ src/Trees/LevelOrderTraversal.java
@@ -0,0 +1,43 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        System.out.println(levelOrder(root));
+    }
+
+    private static List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null)
+            return null;
+
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = q.remove();
+                currentLevel.add(current.data);
+                if (current.left != null)
+                    q.add(current.left);
+                if (current.right != null)
+                    q.add(current.right);
+            }
+        res.add(currentLevel);
+        }
+        return res;
+    }
+}

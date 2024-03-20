d5b016fa48160f0e86b51f0efced74abf8f41c27
nascentnidhisingh <nascentnidhisingh@gmaile.com>
2024-03-20 19:05:52 +0530

    Added BFS implementation

--- src/Graph/DetectCycleInDirectedGraph.java
+++ src/Graph/DetectCycleInDirectedGraph.java
@@ -1,7 +1,9 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DetectCycleInDirectedGraph {
     public static void main(String[] args) {
@@ -24,6 +26,7 @@ public class DetectCycleInDirectedGraph {
         adj.get(10).add(8);
         adj.get(8).add(2);
 
+        topoSort(adj, node);
         boolean cycle = false;
         boolean[] vis = new boolean[node + 1];
         boolean[] pathVis = new boolean[node + 1];
@@ -53,5 +56,34 @@ public class DetectCycleInDirectedGraph {
         }
         pathVis[node] = false;
         return false;
+    }
+
+    public static void topoSort(List<List<Integer>> adj, int V) {
+        int[] indegree = new int[V + 1];
+
+        for (int i = 0; i <= V; i++) {
+            for (int current : adj.get(i)) {
+                indegree[current]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        int count = 0;
+        while(!q.isEmpty()) {
+            int current = q.peek();
+            q.remove();
+            count++;
+            for (int i : adj.get(current)) {
+                indegree[i]--;
+                if (indegree[i] == 0) {
+                    q.add(i);
+                }
+            }
+        }
+        System.out.println(count != V);
     }
 }

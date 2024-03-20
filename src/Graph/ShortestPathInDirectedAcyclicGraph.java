3d2c3e2b730f8e290d186a22fb7de675b6c73abe
nascentnidhisingh <nascentnidhisingh@gmaile.com>
2024-03-20 19:04:58 +0530

    Shortest Path In Directed Acyclic Graph problem

---
+++ src/Graph/ShortestPathInDirectedAcyclicGraph.java
@@ -0,0 +1,78 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class ShortestPathInDirectedAcyclicGraph {
+    public static void main(String[] args) {
+        int n = 6, m = 7;
+        int[][] edge = {{0,1,2},{0,4,1},{4,5,4},{4,2,2},{1,2,3},{2,3,6},{5,3,1}};
+        ShortestPathInDirectedAcyclicGraph obj = new ShortestPathInDirectedAcyclicGraph();
+        int[] res = obj.shortestPath(n, m, edge);
+        for (int i = 0; i < n; i++) {
+            System.out.print(res[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private int[] shortestPath(int n, int m, int[][] edge) {
+        List<List<Pairs>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Pairs> temp = new ArrayList<>();
+            adj.add(temp);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = edge[i][0];
+            int v = edge[i][1];
+            int wt = edge[i][2];
+            adj.get(u).add(new Pairs(v, wt));
+        }
+        Stack<Integer> stack = new Stack<>();
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(adj, i, vis, stack);
+            }
+        }
+
+        int[] dis = new int[n];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[0] = 0;
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            for (int i = 0; i < adj.get(current).size(); i++) {
+                int v = adj.get(current).get(i).first;
+                int wt = adj.get(current).get(i).second;
+                if ((dis[current] + wt) < dis[v]) {
+                    dis[v] = dis[current] + wt;
+                }
+            }
+        }
+
+        return dis;
+    }
+
+    private static void dfs(List<List<Pairs>> adj, int node, boolean[] vis, Stack stack) {
+        vis[node] = true;
+        for (int i = 0; i < adj.get(node).size(); i++) {
+            int v = adj.get(node).get(i).first;
+            if (!vis[v]) {
+                dfs(adj, v, vis, stack);
+            }
+        }
+        stack.push(node);
+    }
+}
+
+class Pairs {
+    int first;
+    int second;
+    Pairs(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
